d0c9e084ac3cd17f97a5b8913543ea88538bc941
MounirMahran <mounirmahran21@gmail.com>
2023-04-23 21:58:21 +0200

    Created The Form

---
+++ To-Do List/To-Do List/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = checkedListBox1.Items.Count - 1; i >= 0; i--)
+            {
+                checkedListBox1.Items.RemoveAt(i);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for(int i= checkedListBox1.Items.Count - 1;i >= 0; i--)
+            {
+                if (!checkedListBox1.GetItemChecked(i))
+                {
+                    checkedListBox1.SetItemChecked(i, true);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = checkedListBox1.Items.Count - 1; i >= 0; i--)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    checkedListBox1.SetItemChecked(i, false);
+                }
+            }
+        }
+    }
+}
